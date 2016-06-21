2f2cacfbe9d75afb17ec8b64a9589a596a9f6e46
xufangzhen <xu_fangzhen@126.com>
2016-06-22 00:13:36 +0800

    优化代码

--- app/src/main/java/com/newbieguide/GuideView.java
+++ app/src/main/java/com/newbieguide/GuideView.java
@@ -18,119 +18,104 @@ import java.util.List;
  */
 public class GuideView extends RelativeLayout {
 
-    private int mBgColor = 0xb2000000;
-    private float mStrokeWidth;
-    private Paint mPaint;
-    private Bitmap mBitmap;
-    private RectF mBitmapRect;
-    private Canvas mCanvas;
-    private List<HoleBean> mHoleList;
-    private BlurMaskFilter bmf;
-    private PorterDuffXfermode pdf;
+	private int mBgColor = 0xb2000000;
+	private float mStrokeWidth;
+	private Paint mPaint;
+	private Bitmap mBitmap;
+	private RectF mBitmapRect;
+	private Canvas mCanvas;
+	private List<HoleBean> mHoleList;
+	private PorterDuffXfermode pdf;
 
-    public GuideView(Context context) {
-        super(context);
-        init();
-    }
+	public GuideView(Context context) {
+		super(context);
+		init();
+	}
 
-    public GuideView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
+	public GuideView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
 
-    public GuideView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-    }
+	public GuideView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init();
+	}
 
-    private void init() {
-        pdf = new PorterDuffXfermode(PorterDuff.Mode.CLEAR);
-        bmf = new BlurMaskFilter(10, BlurMaskFilter.Blur.INNER);
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setColor(mBgColor);
-        setClickable(true);
-        setWillNotDraw(false);
-    }
+	private void init() {
+		pdf = new PorterDuffXfermode(PorterDuff.Mode.CLEAR);
+		mPaint = new Paint();
+		mPaint.setAntiAlias(true);
+		mPaint.setColor(mBgColor);
+		mPaint.setMaskFilter(new BlurMaskFilter(10, BlurMaskFilter.Blur.INNER));
+		mBitmapRect = new RectF();
+		setClickable(true);
+		setWillNotDraw(false);
+	}
 
-    public void setDate(List<HoleBean> holeList) {
-        mHoleList = holeList;
-        if(mHoleList != null && mHoleList.size() > 0) {
-            mBitmapRect = mHoleList.get(0).getRectF();
-            for (HoleBean hole : mHoleList) {
-                mBitmapRect.left = Math.min(mBitmapRect.left, hole.getRectF().left);
-                mBitmapRect.top = Math.min(mBitmapRect.top, hole.getRectF().top);
-                mBitmapRect.right = Math.max(mBitmapRect.right, hole.getRectF().right);
-                mBitmapRect.bottom = Math.max(mBitmapRect.bottom, hole.getRectF().bottom);
-            }
-        }
-        mStrokeWidth = Math.max(Math.max(mBitmapRect.left, mBitmapRect.top), Math.max(ScreenUtils.getScreenWidth(getContext()) -
-                mBitmapRect.left -
-                mBitmapRect.width(), ScreenUtils.getScreenHeight(getContext()) -
-                mBitmapRect.top - mBitmapRect.height())) + 1;
-        if(mBitmapRect.width() > 0 && mBitmapRect.height() > 0) {
-            mBitmap = Bitmap.createBitmap((int) mBitmapRect.width(), (int) mBitmapRect.height(), Bitmap.Config.ARGB_8888);
-        } else {
-            mBitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888);
-            throw new UnsupportedOperationException("需要高亮的view尚未加载完，请调整适当的时机或者延迟");
-        }
-        mCanvas = new Canvas(mBitmap);
-        mCanvas.drawColor(mBgColor);
-    }
+	public void setDate(List<HoleBean> holeList) {
+		mHoleList = holeList;
+		if (mHoleList != null && !mHoleList.isEmpty()) {
+			for (HoleBean hole : mHoleList) {
+				mBitmapRect.union(hole.getRectF());
+			}
+		}
+		mStrokeWidth = Math.max(Math.max(mBitmapRect.left, mBitmapRect.top), Math.max(ScreenUtils.getScreenWidth(getContext()) -
+				mBitmapRect.right, ScreenUtils.getScreenHeight(getContext()) - mBitmapRect.bottom));
+		if (mBitmapRect.width() > 0 && mBitmapRect.height() > 0) {
+			mBitmap = Bitmap.createBitmap((int) mBitmapRect.width(), (int) mBitmapRect.height(), Bitmap.Config.ARGB_8888);
+		} else {
+			mBitmap = Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888);
+			throw new UnsupportedOperationException("需要高亮的view尚未加载完，请调整适当的时机或者延迟");
+		}
+		mCanvas = new Canvas(mBitmap);
+		mCanvas.drawColor(mBgColor);
+	}
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        if(mHoleList != null && mHoleList.size() > 0) {
-            mPaint.setXfermode(pdf);
-            mPaint.setMaskFilter(bmf);
-            mPaint.setStyle(Paint.Style.FILL);
-            for (HoleBean hole : mHoleList) {
-                switch (hole.getType()) {
-                    case HoleBean.TYPE_CIRCLE:
-                        mCanvas.drawCircle(hole.getCenterX() - mBitmapRect.left, hole.getCenterY() - mBitmapRect.top, hole.getRadius(),
-                                mPaint);
-                        break;
-                    case HoleBean.TYPE_RECTANGLE:
-                        mCanvas.drawRect(modifyRect(hole.getRectF()), mPaint);
-                        break;
-                    case HoleBean.TYPE_OVAL:
-                        mCanvas.drawOval(modifyRect(hole.getRectF()), mPaint);
-                        break;
-                }
-            }
-            canvas.drawBitmap(mBitmap, mBitmapRect.left, mBitmapRect.top, null);
-            //绘制剩余空间的矩形
-            mPaint.setXfermode(null);
-            mPaint.setMaskFilter(null);
-            mPaint.setStyle(Paint.Style.STROKE);
-            mPaint.setStrokeWidth(mStrokeWidth + 0.1f);
-            canvas.drawRect(fillRect(mBitmapRect), mPaint);
-        }
-    }
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		if (mHoleList != null && mHoleList.size() > 0) {
+			mPaint.setXfermode(pdf);
+			mPaint.setStyle(Paint.Style.FILL);
+			for (HoleBean hole : mHoleList) {
+				RectF rectF = hole.getRectF();
+				rectF.offset(-mBitmapRect.left, -mBitmapRect.top);
+				switch (hole.getType()) {
+					case HoleBean.TYPE_CIRCLE:
+						mCanvas.drawCircle(rectF.centerX(), rectF.centerY(), hole.getRadius(), mPaint);
+						break;
+					case HoleBean.TYPE_RECTANGLE:
+						mCanvas.drawRect(rectF, mPaint);
+						break;
+					case HoleBean.TYPE_OVAL:
+						mCanvas.drawOval(rectF, mPaint);
+						break;
+				}
+			}
+			canvas.drawBitmap(mBitmap, mBitmapRect.left, mBitmapRect.top, null);
+			//绘制剩余空间的矩形
+			mPaint.setXfermode(null);
+			mPaint.setStyle(Paint.Style.STROKE);
+			mPaint.setStrokeWidth(mStrokeWidth + 0.1f);
+			canvas.drawRect(fillRect(mBitmapRect), mPaint);
+		}
+	}
 
-    private RectF modifyRect(RectF rectF) {
-        rectF.left -= mBitmapRect.left;
-        rectF.right -= mBitmapRect.left;
-        rectF.top -= mBitmapRect.top;
-        rectF.bottom -= mBitmapRect.top;
-        return rectF;
-    }
+	private RectF fillRect(RectF rectF) {
+		RectF fillRect = new RectF();
+		fillRect.left = rectF.left - mStrokeWidth / 2;
+		fillRect.top = rectF.top - mStrokeWidth / 2;
+		fillRect.right = rectF.right + mStrokeWidth / 2;
+		fillRect.bottom = rectF.bottom + mStrokeWidth / 2;
+		return fillRect;
+	}
 
-    private RectF fillRect(RectF rectF) {
-        RectF fillRect = new RectF();
-        fillRect.left = rectF.left - mStrokeWidth / 2;
-        fillRect.top = rectF.top - mStrokeWidth / 2;
-        fillRect.right = rectF.right + mStrokeWidth / 2;
-        fillRect.bottom = rectF.bottom + mStrokeWidth / 2;
-        return fillRect;
-    }
-
-    public void recycler() {
-        if(mBitmap != null) {
-            mBitmap.recycle();
-            mBitmap = null;
-        }
-    }
+	public void recycler() {
+		if (mBitmap != null) {
+			mBitmap.recycle();
+			mBitmap = null;
+		}
+	}
 
 }
--- app/src/main/java/com/newbieguide/HoleBean.java
+++ app/src/main/java/com/newbieguide/HoleBean.java
@@ -8,55 +8,37 @@ import android.view.View;
  */
 public class HoleBean {
 
-    public static final int TYPE_CIRCLE = 0;
-    public static final int TYPE_RECTANGLE = TYPE_CIRCLE + 1;
-    public static final int TYPE_OVAL = TYPE_RECTANGLE + 1;
+	public static final int TYPE_CIRCLE = 0;
+	public static final int TYPE_RECTANGLE = TYPE_CIRCLE + 1;
+	public static final int TYPE_OVAL = TYPE_RECTANGLE + 1;
 
-    private View mHole;
-    private int mType;
-    private int[] mLocation = new int[2];
+	private View mHole;
+	private int mType;
 
-    public HoleBean(View hole, int type) {
-        this.mHole = hole;
-        this.mType = type;
-    }
+	public HoleBean(View hole, int type) {
+		this.mHole = hole;
+		this.mType = type;
+	}
 
-    public int getCenterX() {
-        int centerX = 0;
-        if(mHole != null) {
-            mHole.getLocationOnScreen(mLocation);
-            centerX = mLocation[0] + mHole.getWidth() / 2;
-        }
-        return centerX;
-    }
+	public int getRadius() {
+		return mHole != null ? Math.min(mHole.getWidth(), mHole.getHeight()) / 2 : 0;
+	}
 
-    public int getCenterY() {
-        int centerY = 0;
-        if(mHole != null) {
-            mHole.getLocationOnScreen(mLocation);
-            centerY = mLocation[1] + mHole.getHeight() / 2;
-        }
-        return centerY;
-    }
+	public RectF getRectF() {
+		RectF rectF = new RectF();
+		if (mHole != null) {
+			int[] location = new int[2];
+			mHole.getLocationOnScreen(location);
+			rectF.left = location[0];
+			rectF.top = location[1];
+			rectF.right = location[0] + mHole.getWidth();
+			rectF.bottom = location[1] + mHole.getHeight();
+		}
+		return rectF;
+	}
 
-    public int getRadius() {
-        return mHole != null ? Math.min(mHole.getWidth(), mHole.getHeight()) / 2 : 0;
-    }
-
-    public RectF getRectF() {
-        RectF rectF = new RectF();
-        if(mHole != null) {
-            mHole.getLocationOnScreen(mLocation);
-            rectF.left = mLocation[0];
-            rectF.top = mLocation[1];
-            rectF.right = mLocation[0] + mHole.getWidth();
-            rectF.bottom = mLocation[1] + mHole.getHeight();
-        }
-        return rectF;
-    }
-
-    public int getType() {
-        return mType;
-    }
+	public int getType() {
+		return mType;
+	}
 
 }
